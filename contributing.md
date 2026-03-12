2b96b3b37d1c34e3fd5ddfe47d170d892763d180
Brian Pontarelli <brian@fusionauth.io>
2026-03-12 16:45:57 -0600

    Final tweaks

--- contributing.md
+++ contributing.md
@@ -4,22 +4,29 @@ title: Contributing to Savant
 description: Find a bug? Have a pull request? Got a new feature idea? Great!
 ---
 
-Please submit all bugs, feature requests and feedback to us using the Github issue tracking system for each library and project:
+Please submit all bugs, feature requests and feedback to us using the Github issue tracking system for each library and plugin:
 
-* [Savant Core (including the runtime)](https://github.com/savant-build/savant-core/)
+**Core Libraries**
+
+* [Savant Version](https://github.com/savant-build/savant-version/)
+* [Savant Utils](https://github.com/savant-build/savant-utils/)
+* [Savant IO](https://github.com/savant-build/savant-io/)
 * [Savant Dependency Management](https://github.com/savant-build/savant-dependency-management/)
+* [Savant Core (including the runtime)](https://github.com/savant-build/savant-core/)
+
+**Plugins**
+
 * [Database Plugin](https://github.com/savant-build/database-plugin/)
-* [Debian Plugin](https://github.com/savant-build/debian-plugin/)
+* [Debian Plugin](https://github.com/savant-build/deb-plugin/)
 * [Dependency Plugin](https://github.com/savant-build/dependency-plugin/)
 * [File Plugin](https://github.com/savant-build/file-plugin/)
 * [Groovy Plugin](https://github.com/savant-build/groovy-plugin/)
 * [Groovy TestNG Plugin](https://github.com/savant-build/groovy-testng-plugin/)
 * [IDEA Plugin](https://github.com/savant-build/idea-plugin/)
-* [Java Plugin](https://github.com/savant-build/idea-plugin/)
+* [Java Plugin](https://github.com/savant-build/java-plugin/)
 * [Java TestNG Plugin](https://github.com/savant-build/java-testng-plugin/)
+* [Linter Plugin](https://github.com/savant-build/linter-plugin/)
+* [POM Plugin](https://github.com/savant-build/pom-plugin/)
 * [Release Git Plugin](https://github.com/savant-build/release-git-plugin/)
-* [Spock Plugin](https://github.com/savant-build/spock-plugin/)
 * [Tomcat Plugin](https://github.com/savant-build/tomcat-plugin/)
 * [Webapp Plugin](https://github.com/savant-build/webapp-plugin/)
-
-You can also ask questions and get support on Stack Overflow using the tag **savant-build**.
--- js/darkmode.js
+++ js/darkmode.js
@@ -3,25 +3,72 @@
   var theme = localStorage.getItem('theme');
   if (theme === 'light') {
     document.documentElement.classList.remove('dark');
-  } else {
+  } else if (theme === 'dark') {
     document.documentElement.classList.add('dark');
+  } else {
+    // System preference
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
   }
+
+  // Listen for system preference changes when in system mode
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', function(e) {
+    if (!localStorage.getItem('theme')) {
+      if (e.matches) {
+        document.documentElement.classList.add('dark');
+      } else {
+        document.documentElement.classList.remove('dark');
+      }
+      updateThemeIcons();
+    }
+  });
 })();
 
-function toggleDarkMode() {
-  var html = document.documentElement;
-  if (html.classList.contains('dark')) {
-    html.classList.remove('dark');
-    localStorage.setItem('theme', 'light');
+function cycleTheme() {
+  var current = localStorage.getItem('theme');
+  var next;
+  if (current === 'light') {
+    next = 'dark';
+  } else if (current === 'dark') {
+    next = null; // system
   } else {
-    html.classList.add('dark');
-    localStorage.setItem('theme', 'dark');
+    next = 'light';
   }
-  // Update toggle button icons
-  document.querySelectorAll('.dark-icon').forEach(function(el) {
-    el.style.display = html.classList.contains('dark') ? 'none' : 'block';
+
+  if (next) {
+    localStorage.setItem('theme', next);
+  } else {
+    localStorage.removeItem('theme');
+  }
+
+  // Apply
+  if (next === 'light') {
+    document.documentElement.classList.remove('dark');
+  } else if (next === 'dark') {
+    document.documentElement.classList.add('dark');
+  } else {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }
+
+  updateThemeIcons();
+}
+
+function updateThemeIcons() {
+  var theme = localStorage.getItem('theme');
+  document.querySelectorAll('.theme-icon-light').forEach(function(el) {
+    el.style.display = theme === 'light' ? 'block' : 'none';
   });
-  document.querySelectorAll('.light-icon').forEach(function(el) {
-    el.style.display = html.classList.contains('dark') ? 'block' : 'none';
+  document.querySelectorAll('.theme-icon-dark').forEach(function(el) {
+    el.style.display = theme === 'dark' ? 'block' : 'none';
+  });
+  document.querySelectorAll('.theme-icon-system').forEach(function(el) {
+    el.style.display = !theme ? 'block' : 'none';
   });
 }
